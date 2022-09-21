60bc7553d0b40ed23c0edcac243301fdfc98e5fa
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-21 15:32:37 +0300

    3.6 tabl

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+print "<table style=\"border:solid;\">";
+for ($i = 0; $i < 10; $i++) {
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"><img src=\"images/1.jpg\"></td></tr>";
+}
+print "</table>";
