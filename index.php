5aeecbe80e9ed2899f3d4d084d4e988ac885c80f
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-21 16:50:14 +0300

    Add width

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 print "<table style=\"border:solid;\">";
 for ($i = 1; $i < 11; $i++) {
-    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"><img src=\"images/img_$i.jpg\"></td></tr><style=\"\">";
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"><img src=\"images/img_$i.jpg\"width=\"300\"></td></tr>";
 }
 print "</table>";
