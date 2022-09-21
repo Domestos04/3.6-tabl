39fb3891bb57aff85550d3c6db8391e2018ad3d6
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-21 20:31:37 +0300

    Add if

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 print "<table style=\"border:solid;\">";
 for ($i = 1; $i < 11; $i++) {
+    if($i == 0);
     print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"><img src=\"images/img_$i.jpg\"width=\"300\"></td></tr>";
 }
 print "</table>";
