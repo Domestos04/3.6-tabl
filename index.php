dd80c90b718423e89b98f0eae32b7904ca1d71b5
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-21 16:37:17 +0300

    Add images

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 print "<table style=\"border:solid;\">";
-for ($i = 0; $i < 10; $i++) {
-    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"><img src=\"images/1.jpg\"></td></tr>";
+for ($i = 1; $i < 11; $i++) {
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"><img src=\"images/img_$i.jpg\"></td></tr><style=\"\">";
 }
 print "</table>";
